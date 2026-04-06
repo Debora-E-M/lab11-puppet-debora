77c836ce95fa540e3fc8f224356b82d119925436
Debora Mekonen <mekon044@umn.edu>
2026-04-06 15:43:24 -0500

    Update server_users_groups.pp

--- server_users_groups.pp
+++ server_users_groups.pp
@@ -1,1 +1,14 @@
+group { 'group01':
+  ensure => present,
+}
 
+group { 'group02':
+  ensure => present,
+}
+
+user { 'user04':
+  ensure => present,
+  shell  => '/bin/bash',
+  home   => '/home/user04',
+  managehome => true,
+}
