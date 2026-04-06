304a1ea69e7f66e61dd91d7e471be492701e8c8d
Debora Mekonen <mekon044@umn.edu>
2026-04-06 15:42:59 -0500

    Update lamp_stack_server.pp

--- lamp_stack_server.pp
+++ lamp_stack_server.pp
@@ -1,1 +1,25 @@
+package { 'apache2':
+  ensure => installed,
+}
 
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
+
+package { 'php':
+  ensure => installed,
+}
+
+package { 'libapache2-mod-php':
+  ensure => installed,
+}
+
+package { 'mariadb-server':
+  ensure => installed,
+}
+
+service { 'mariadb':
+  ensure => running,
+  enable => true,
+}
